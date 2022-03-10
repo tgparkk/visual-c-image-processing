8a8daae5fe8684c5558d0a4658a6ffe97e8999ce
tgparkk <sttgpark@gmail.com>
2022-03-10 20:00:31 +0900

    7.1 장 p311

--- ImageTool/ImageTool/IppImage/IppEnhance.cpp
+++ ImageTool/ImageTool/IppImage/IppEnhance.cpp
@@ -119,3 +119,97 @@ void IppHistogramEqualization(IppByteImage& img)
 		p[i] = static_cast<BYTE>(limit(cdf[p[i]] * 255));
 	}
 }
+
+bool IppAdd(IppByteImage& img1, IppByteImage& img2, IppByteImage& img3)
+{
+	int w = img1.GetWidth();
+	int h = img1.GetHeight();
+
+	if (w != img2.GetWidth() || h != img2.GetHeight())
+		return false;
+
+	img3.CreateImage(w, h);
+
+	int size = img3.GetSize();
+	BYTE* p1 = img1.GetPixels();
+	BYTE* p2 = img2.GetPixels();
+	BYTE* p3 = img3.GetPixels();
+
+	for (int i = 0; i < size; i++)
+	{
+		p3[i] = limit(p1[i] + p2[i]);
+	}
+
+	return true;
+}
+
+bool IppSub(IppByteImage& img1, IppByteImage& img2, IppByteImage& img3)
+{
+	int w = img1.GetWidth();
+	int h = img1.GetHeight();
+
+	if (w != img2.GetWidth() || h != img2.GetHeight())
+		return false;
+
+	img3.CreateImage(w, h);
+
+	int size = img3.GetSize();
+	BYTE* p1 = img1.GetPixels();
+	BYTE* p2 = img2.GetPixels();
+	BYTE* p3 = img3.GetPixels();
+
+	for (int i = 0; i < size; i++)
+	{
+		p3[i] = limit(p1[i] - p2[i]);
+	}
+
+	return true;
+}
+
+bool IppAve(IppByteImage& img1, IppByteImage& img2, IppByteImage& img3)
+{
+	int w = img1.GetWidth();
+	int h = img1.GetHeight();
+
+	if (w != img2.GetWidth() || h != img2.GetHeight())
+		return false;
+
+	img3.CreateImage(w, h);
+
+	int size = img3.GetSize();
+	BYTE* p1 = img1.GetPixels();
+	BYTE* p2 = img2.GetPixels();
+	BYTE* p3 = img3.GetPixels();
+
+	for (int i = 0; i < size; i++)
+	{
+		p3[i] = (p1[i] + p2[i]) / 2;
+	}
+
+	return true;
+}
+
+bool IppDiff(IppByteImage& img1, IppByteImage& img2, IppByteImage& img3)
+{
+	int w = img1.GetWidth();
+	int h = img1.GetHeight();
+
+	if (w != img2.GetWidth() || h != img2.GetHeight())
+		return false;
+
+	img3.CreateImage(w, h);
+
+	int size = img3.GetSize();
+	BYTE* p1 = img1.GetPixels();
+	BYTE* p2 = img2.GetPixels();
+	BYTE* p3 = img3.GetPixels();
+
+	int diff;
+	for (int i = 0; i < size; i++)
+	{
+		diff = p1[i] - p2[i];
+		p3[i] = static_cast<BYTE>((diff >= 0) ? diff : -diff);
+	}
+
+	return true;
+}
--- ImageTool/ImageTool/IppImage/IppEnhance.h
+++ ImageTool/ImageTool/IppImage/IppEnhance.h
@@ -15,3 +15,11 @@ void IppHistogram(IppByteImage& img, float histo[256]);
 void IppHistogramStretching(IppByteImage& img);
 
 void IppHistogramEqualization(IppByteImage& img);
+
+bool IppAdd(IppByteImage& img1, IppByteImage& img2, IppByteImage& img3);
+
+bool IppSub(IppByteImage& img1, IppByteImage& img2, IppByteImage& img3);
+
+bool IppAve(IppByteImage& img1, IppByteImage& img2, IppByteImage& img3);
+
+bool IppDiff(IppByteImage& img1, IppByteImage& img2, IppByteImage& img3);
